6b04c6719c4e38f39cbab1ab426be47ffb053b2d
Justin Barabander <justinbarabander@Justins-MacBook-Pro.local>
2015-08-11 17:56:45 -0400

    Started implementing wiki routes

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var swig = require('swig');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var wiki = require('./routes/wiki');
 
 var app = express();
 
 // view engine setup
-app.engine('html' swig.renderFile);
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/wiki', wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/index.js
+++ models/index.js
@@ -5,17 +5,21 @@ var db = mongoose.conection;
 db.on('error', console.error.bind(console, 'mongodb connection error: '));
 
 var pageSchema = new mongoose.Schema({
-  title:    {type: String, require: true},
-  urlTitle: {type: String, require: true},
-  content:  {type: String, require: true},
-  status:   String,
+  title:    {type: String, required: true},
+  urlTitle: {type: String, required: true},
+  content:  {type: String, required: true},
+  status:   {type: String, enum: ['open', 'closed']},
   date:     {type: Date, default: Date.now},
   author:   {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
+pageSchema.virtual('route').get(function() {
+  return '/wiki/' + this.urlTitle;
+});
+
 var userSchema = new mongoose.Schema({
-  name: {first: String, last: String},
-  email: String
+  name: {first: {type: String, required: true}, last: {type: String, required: true}},
+  email: {type: String, required: true, unique: true}
 });
 
 var Page = mongoose.model('Page', pageSchema);
